11549c1c90828be4777fcc967039fa87ceb476d4
Sai Surya Teja <saisuryateja3@gmail.com>
2024-10-11 13:47:57 +0530

    Profile Page Enhancements

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,11 +6,27 @@ const Profile = () => {
   const loggedInUser = useSelector((store) => store?.user?.userData);
   const [user, setUser] = useState({});
 
-  // useEffect(() => {
-  //   setUser(loggedInUser);
-  // }, [loggedInUser]);
+  useEffect(() => {
+    setUser(loggedInUser);
+  }, [loggedInUser]);
 
   const handleUpdate = async (e) => {
+    const allowedEditFields = [
+      "firstName",
+      "lastName",
+      "photoUrl",
+      "gender",
+      "age",
+      "about",
+      "skills",
+    ];
+
+    Object.keys(user).forEach((item) => {
+      if (!allowedEditFields.includes(item)) {
+        delete user[item];
+      }
+    });
+
     e.preventDefault();
     try {
       await updateProfile(user);
@@ -20,84 +36,117 @@ const Profile = () => {
   };
 
   return (
-    <div className="flex items-center gap-4">
-      <div className="card bg-base-100 w-96 shadow-xl flex items-center">
+    <div className="flex items-center gap-4 justify-center">
+      <div className="card card-bordered bg-base-100 w-96 shadow-xl flex flex-col items-center p-8">
         <div className="avatar">
           <div className="w-24 rounded-full">
-            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+            <img src={loggedInUser?.photoUrl} alt="user-img" />
           </div>
         </div>
         <div className="card-body">
           <h2 className="card-title">
             {loggedInUser?.firstName} {loggedInUser?.lastName}
           </h2>
+          <h6 className="">{loggedInUser?.emailId}</h6>
+          <h6 className="">{loggedInUser?.gender}</h6>
         </div>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card card-bordered bg-base-100 w-2/3 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Info</h2>
-          <form>
-            <input
-              type="text"
-              placeholder="First name"
-              className="input input-bordered w-full max-w-xs"
-              value={loggedInUser?.firstName}
-              onChange={(e) =>
-                setUser((prev) => {
-                  return { ...prev, firstName: e.target.value };
-                })
-              }
-            />
-            <input
-              type="text"
-              placeholder="Last name"
-              className="input input-bordered w-full max-w-xs"
-              value={user?.lastName}
-              onChange={(e) =>
-                setUser((prev) => {
-                  return { ...prev, lastName: e.target.value };
-                })
-              }
-            />
-            <input
-              type="text"
-              placeholder="About"
-              className="input input-bordered w-full max-w-xs"
-              value={user?.about}
-              onChange={(e) =>
-                setUser((prev) => {
-                  return { ...prev, about: e.target.value };
-                })
-              }
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              className="input input-bordered w-full max-w-xs"
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              className="input input-bordered w-full max-w-xs"
-              value={user?.emailId}
-              onChange={(e) =>
-                setUser((prev) => {
-                  return { ...prev, emailId: e.target.value };
-                })
-              }
-              disabled
-            />
-            <input
-              type="text"
-              placeholder="Skills"
-              className="input input-bordered w-full max-w-xs"
-              value={user?.skills}
-              onChange={(e) =>
-                setUser((prev) => {
-                  return { ...prev, skills: e.target.value };
-                })
-              }
-            />
+          <form className="flex flex-col gap-4">
+            <div className="flex w-full">
+              <div className="flex flex-col gap-3 w-full">
+                <input
+                  type="text"
+                  placeholder="First name"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.firstName}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return { ...prev, firstName: e.target.value };
+                    })
+                  }
+                />
+                <input
+                  type="text"
+                  placeholder="Last name"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.lastName}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return { ...prev, lastName: e.target.value };
+                    })
+                  }
+                />
+                <input
+                  type="text"
+                  placeholder="About"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.about}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return { ...prev, about: e.target.value };
+                    })
+                  }
+                />
+
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.emailId}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return { ...prev, emailId: e.target.value };
+                    })
+                  }
+                  disabled
+                />
+              </div>
+              <div className="flex flex-col gap-3 w-full">
+                <input
+                  type="text"
+                  placeholder="Gender"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.gender}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return { ...prev, gender: e.target.value };
+                    })
+                  }
+                />
+                <input
+                  type="text"
+                  placeholder="Skills"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.skills}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return {
+                        ...prev,
+                        skills: [...prev.skills, e.target.value],
+                      };
+                    })
+                  }
+                />
+                <input
+                  type="text"
+                  placeholder="Photo URL"
+                  className="input input-bordered w-full max-w-xs"
+                  value={user?.photoUrl}
+                  onChange={(e) =>
+                    setUser((prev) => {
+                      return {
+                        ...prev,
+                        photoUrl: e.target.value,
+                      };
+                    })
+                  }
+                />
+              </div>
+            </div>
+
             <button className="btn btn-primary" onClick={handleUpdate}>
               Update
             </button>
